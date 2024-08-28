0012331cc7bae614edfea92b49fb1d2937b9e7df
LARI CABRERA <LARI CABRERA@DESKTOP-QFB41NK>
2024-08-27 20:54:09 -0600

    Agregar archivos de proyecto.

---
+++ Práctica_4_1_.py
@@ -0,0 +1,12 @@
+# Crear una matriz 3x3
+matriz = [[0 for _ in range(3)] for _ in range(3)]
+
+# Escritura en la matriz utilizando ciclos anidados
+for i in range(3):
+    for j in range(3):
+        matriz[i][j] = i + j  # Puedes cambiar esta expresión para otros valores
+
+# Lectura e impresión de la matriz utilizando ciclos anidados
+for i in range(3):
+    for j in range(3):
+        print(f"Elemento en [{i}][{j}]: {matriz[i][j]}")
